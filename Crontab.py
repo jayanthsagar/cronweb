2e2e108b7adcedd5ef290614bc5545173d6e188c
root <root@ip-172-31-31-172.us-west-2.compute.internal>
2017-03-17 00:49:35 +0000

    lists crontab jobs

--- Crontab.py
+++ Crontab.py
@@ -1,9 +1,23 @@
-from pycron import *
-from bash import bash
+#from pycron import *
+#from bash import bash
+import subprocess
+import re
 
 class Crontab:
     def Get_jobs(self):
         f = open("./templates/list.html",'w')
-        output = bash('crontab -l')
-        f.write(str(output))
+        output =subprocess.check_output(["crontab", "-l"])
+#        output = os.system('crontab -l')
+        string =output.split()
+        print string
+        jobs=[]
+        for i in range(0, len(string),6):
+            jobs.append(string[i-1])
+        f.write(str(jobs))
 
+    def run_job(self,job):
+        output =subprocess.check_output(job)
+
+c = Crontab()
+if __name__ == '__main__':
+    c.Get_jobs()
--- cron_server.py
+++ cron_server.py
@@ -19,15 +19,20 @@ import tornado.web
 from tornado.options import define, options
 import Crontab
 import logging
+
 define("port", default=8000, help="run on the given port", type=int)
+
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render('index.html')
+
 class List_of_jobs(tornado.web.RequestHandler):
+
     def get(self):
        c= Crontab.Crontab()
        c.Get_jobs()
        self.render('list.html')
+
 #    def post(self):
 #        post_data = dict(urlparse.parse_qsl(self.request.body))
 #        c = Get_issues.Get_issues()
@@ -35,17 +40,18 @@ class List_of_jobs(tornado.web.RequestHandler):
 #        c.write_all_issues_to_a_file_after_applying_filter(post_data['labels'])
 #        self.render('result.html')
 #
+
 if __name__ == "__main__":
-	tornado.options.parse_command_line()
+        tornado.options.parse_command_line()
 	app = tornado.web.Application(
 		handlers=[
 			(r"/", MainHandler),
-            (r"/list", List_of_jobs),
-		],
-		template_path=os.path.join(os.path.dirname(__file__), "templates"),debug = True)
+                        (r"/list", List_of_jobs),
+		        ],
+		        template_path=os.path.join(os.path.dirname(__file__), "templates"),debug = True)
 	http_server = tornado.httpserver.HTTPServer(app)
 	current_file_path = os.path.dirname(os.path.abspath(__file__))
 	options.port = 8080
-	logging.info("utils_server: It will run on port : "+str(options.port))
+	logging.info("cronweb_server: It will run on port : "+str(options.port))
 	http_server.listen(options.port)
 	tornado.ioloop.IOLoop.instance().start()
