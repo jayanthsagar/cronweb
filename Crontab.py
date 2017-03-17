73b6de0b17e2b8ffa24c90ee05f4a7ae5981b9e1
jayanthsagar <jayanthsagar.t@gmail.com>
2017-03-17 03:12:42 +0000

    modified Crontab

--- Crontab.py
+++ Crontab.py
@@ -15,7 +15,7 @@ class Crontab:
         content='''{% extends "base.html" %}{% block CSS %}body {    background: #FFF;    text-align: center;    font-family: georgia;}#container {    margin: 0 auto;    width: 960px;}img{    display: inline-block;}h1 {    font-size: 4em;    border-bottom: 1px solid #dddddd;    margin: 4%;}h2 {    font-size: 3.5em;    border-bottom: 1px solid #dddddd;    margin: 2%;}p {    font-size: 2em;}.field {     padding: 2%;     margin: 1px;}.textbox {    font-family: monospace;}.textbox:focus {    outline: none;    border: 2px solid #6EA2DE;    box-shadow: 0 0 2px #95B9C7;}label {    margin-left: 1%;}input { display: inline-block;}{% end %}{% block title %}            cronweb{% end %}{% block header %}    List of jobs {% end %}{% block body %}'''+output
         for i in range(0, len(string),6):
             jobs.append(string[i-1])
-            content +='<form action="/list" method="post"><div class="field"><label> <input type="text" value="'+string[i-1]+'" name ="cronjob" id="cronjob" class="textbox"><input type="submit"  value = "run this job" style="width: 80; height: 30"></input></div></form>'
+            content +='<form action="/list" method="post"><div class="field"><label> <input type="text" value="'+string[i-1]+'" name ="cronjob" id="cronjob" class="textbox"><input type="submit"  value = "run this job"></input></div></form>'
         content +='{% end %}'
         f.write(content)
 
